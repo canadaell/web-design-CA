56bb0b293661abc4527b76ffe736cf463b9a9797
Devin <github@zaku.eu.org>
2023-12-04 01:15:15 +0000

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    // Perform your validation here
+    alert('Form submitted!'); // Placeholder for form submission
+  });
